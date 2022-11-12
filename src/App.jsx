c31835b394cdbfba24fff964ccb8dd796b795c7c
Deni <06denidzh@gmail.com>
2022-11-12 17:40:40 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,20 @@
 import './index.css'; 
 import React from 'react';
 import {useState} from 'react';
-import TodoList from './TodoList';
+import TodoList, {addTodo} from './TodoList';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 
 function App() {
-  const [todo, setTodo] = useState ([]);
-
- 
-
   
+    
 
- // const presshandler = () => {
- //   setNote((prevNote) => {
- //     return prevNote.filter(note => note.id != id)
- //   })
- // }
-     
+    
   return (
    <div className='todoo'>
     <header>
-      <h1 className="title">Количество задач № 1</h1>
+      <h1 className="title">Количество задач №{TodoList} </h1>
     </header>
     <TodoList/>
    </div>
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import plus from '../src/static/img/plus.png'
 
 
 
